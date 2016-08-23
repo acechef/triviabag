9b7066376e03b63a373d762b86a7ddeee319aae9
strong-ge <simba_cc@163.com>
2016-08-23 22:28:00 +0800

    auth

--- app/models.py
+++ app/models.py
@@ -1,10 +1,34 @@
+# coding=utf-8
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password can not read')
+
+    @password.setter
+    def password(self, password):
+        """
+        生成hash类型的密码串
+        :param password:
+        :return:
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        验证密码登陆用
+        :param password:
+        :return:
+        """
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
