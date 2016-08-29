2cfe822eefad3d4d88a4b295373e2cf65705bd5f
strong-ge <simba_cc@163.com>
2016-08-29 22:45:57 +0800

    静态文件

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+moment = Moment()
+
 login_manage = LoginManager()
 login_manage.session_protection ='strong'
 login_manage.login_view = 'auth.login'
@@ -19,6 +22,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manage.init_app(app)
+    moment.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
