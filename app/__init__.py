18aa5616345f888945c4848fef9802bd0dd593ac
strong-ge <simba_cc@163.com>
2016-09-27 22:09:11 +0800

    邮件验证

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from config import config
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
+mail = Mail()
 
 login_manage = LoginManager()
 login_manage.session_protection ='strong'
@@ -20,6 +22,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    mail.init_app(app)
     db.init_app(app)
     login_manage.init_app(app)
     moment.init_app(app)
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 from . import db, login_manage
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 # from flask_wtf import Form
 
 
@@ -11,6 +13,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.BOOLEAN, default=False)
 
     @property
     def password(self):
@@ -32,6 +35,22 @@ class User(UserMixin, db.Model):
         :return:
         """
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dump({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.load(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
